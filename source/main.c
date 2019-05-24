d6d9280412b165267756c14df18ae4b526f17944
IOSpaceBar <48366791+IOSpaceBar@users.noreply.github.com>
2019-05-23 22:51:03 -0500

    Added a few things that are useful
    1. Fully working menu system
    2. Commented most of my code
    3. Added a basic exit application selection, it makes sure to exit all previously called methods.

--- source/main.c
+++ source/main.c
@@ -18,16 +18,52 @@ int main(int argc, char **argv)
 	Initialize();
    //END: STARTUP OPERATION
    
+   //MARKER: MENU VALUES
+   int menu1 = 2;
+   int menu2 = 3;
+   int current = 2;
+   printf("GRAVEL by IOSpaceBar\n");
+   //MARKER RENDER MENU
+   printf("\n\x1b[2;4HInstall");
+   printf("\n\x1b[3;4HExit");
+   printf("\x1b[%d;2H>", current);
+   
     while(appletMainLoop())
     {
         hidScanInput();
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+		//MARK: MENU MOVE CODE
+		if (kDown & KEY_UP) 
+		{
+			printf("\x1b[%d;2H ", current);
+			current = current - 1;
+			if (current < 2) current = 3;
+				printf ("\n");
+			    printf ("\x1b[%d;2H>", current);		
+			}
 
+			if (kDown & KEY_DOWN) {
+				printf("\x1b[%d;2H ", current);
+			current = current + 1; 
+			if (current > 3) current = 2;
+				printf("\n");
+				printf("\x1b[%d;2H>", current);
+		}
+		//MARK: MENU ENTER SELECTION CODE
+		if(kDown & KEY_A & current == 2){
+			//TODO: INSTALL MENU
+		}
+		if(kDown & KEY_A & current == 3){
+			//NOTE: DOING "BREAK;" EXITS THE MAIN LOOP ESSENTIALLY CLOSING THE PROGRAM.
+			break;
+		}
         if (kDown & KEY_PLUS) break;
 
         consoleUpdate(NULL);
     }
 
     consoleExit(NULL);
+	splExit();
+	romfsExit();
     return 0;
 }
