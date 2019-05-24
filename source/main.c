d923e29983463fcb77e3f90e7026b26cbfcca959
IOSpaceBar <48366791+IOSpaceBar@users.noreply.github.com>
2019-05-23 22:33:37 -0500

    Initial Code + Startup operation
    This commit includes
    1. An initialize function (STARTUP OPERATION) which can be called at any point.
    2. Codebase (Main Function & a main loop)

---
+++ source/main.c
@@ -0,0 +1,33 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <switch.h>
+void Initialize(){
+	   //Initialize console
+    consoleInit(NULL);   
+	  //Initialize SPL for system functions
+    splInitialize();
+	 //Initialize romfs (ROM FILE SYSTEM)
+	romfsInit();
+	 //return back to main but with null
+	 return;
+   }
+int main(int argc, char **argv)
+{
+   
+   //START: STARTUP OPERATION
+	Initialize();
+   //END: STARTUP OPERATION
+    while(appletMainLoop())
+    {
+        hidScanInput();
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+        if (kDown & KEY_PLUS) break;
+
+        consoleUpdate(NULL);
+    }
+
+    consoleExit(NULL);
+    return 0;
+}
