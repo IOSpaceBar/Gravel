734ae0dec23dcc6fc222624b42ac24554193a715
LordApple <lordapplz@protonmail.com>
2019-05-27 20:05:53 +0200

    Basically a rewrite
    Create a .gitignore
    Fixed menu
    Removed useless stuff
    Fixed bugs
    More stuff

--- source/main.c
+++ source/main.c
@@ -1,91 +1,75 @@
 #include <string.h>
 #include <stdio.h>
 #include <switch.h>
-void Initialize(){
+
+void initialize(){
 	   //MARK: Initialize console
     consoleInit(NULL);   
 	  //MARK: Initialize SPL for system functions
     splInitialize();
-	 //MARK: Initialize romfs (ROM FILE SYSTEM)
-	romfsInit();
-	 //MARK: return back to main but with null
-	 return;
+}
+
+void updateConsole(int current){
+	consoleClear();
+	printf("GRAVEL by IOSpaceBar\n");
+   	printf("\n\x1b[2;4HInstall");
+   	printf("\n\x1b[3;4HExit");
+	printf ("\x1b[%d;2H>", current);
 }
 
 void generateDirectories(){
-	//MARK: RESULT = RC (0)
-	Result rc = 0;
-	//MARK: FILE SYSTEM
-    FsFileSystem FS;
-    
-    if (R_FAILED(rc = fsMountSdcard(&FS)))
-    {
-        printf("Gravel: Failed to mount the SD File System / !\n");
-        return rc;
-    }
-
     mkdir("/Gravel/", 0700);
 	mkdir("/Gravel/packages/", 0700);
 	mkdir("/Gravel/header/", 0700);
-	//CLOSE &FS FOR SAFETY.
-	fsFsClose(&FS);
-	//MARK: CREATE HEADER DATA 
-	FILE *fp;
-	fp=fopen("sdmc:/Gravel/header/header.gravel","w");
-	fclose(fp);
+	fclose(fopen("sdmc:/Gravel/header/header.gravel","w"));
 } 
+
 int main(int argc, char **argv)
 {
    
    //START: STARTUP OPERATION
-	Initialize();
+	initialize();
 	FILE *f = fopen("sdmc:/Gravel/header/header.gravel", "rb");
-        if (f == NULL) {
+        if (!f) {
 			//NOTE: If the header data (what we use to detect if this is the first time ran on this SD Card) doesnt exist, generate dir's
             generateDirectories();
             
-        } else {
-            //MARK: RETURN
-			return;
-        }
+        } else 
+			fclose(f);
 	//END: STARTUP OPERATION
    
    //MARKER: MENU VALUES
-   int menu1 = 2;
-   int menu2 = 3;
    int current = 2;
-   printf("GRAVEL by IOSpaceBar\n");
-   //MARKER RENDER MENU
-   printf("\n\x1b[2;4HInstall");
-   printf("\n\x1b[3;4HExit");
-   printf("\x1b[%d;2H>", current);
-   
+   int options = 3;
+
+	updateConsole(current);
+
     while(appletMainLoop())
     {
         hidScanInput();
         u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-		//MARK: MENU MOVE CODE
-		if (kDown & KEY_UP) 
-		{
-			printf("\x1b[%d;2H ", current);
-			current = current - 1;
-			if (current < 2) current = 3;
-				printf ("\n");
-			    printf ("\x1b[%d;2H>", current);		
+
+		if (kDown & KEY_UP) {
+			current += 1;
+			if (current > options) {
+				current = 2;
 			}
 
-			if (kDown & KEY_DOWN) {
-				printf("\x1b[%d;2H ", current);
-			current = current + 1; 
-			if (current > 3) current = 2;
-				printf("\n");
-				printf("\x1b[%d;2H>", current);
+			updateConsole(current);
+		}
+		else if (kDown & KEY_DOWN) {
+			current -= 1;
+			if (current < options && current == 1) {
+				current = options;
+			}
+
+			updateConsole(current);
 		}
 		//MARK: MENU ENTER SELECTION CODE
-		if(kDown & KEY_A & current == 2){
+		if(kDown & KEY_A && current == 2){
 			//TODO: INSTALL MENU
 		}
-		if(kDown & KEY_A & current == 3){
+		if(kDown & KEY_A && current == 3){
 			//NOTE: DOING "BREAK;" EXITS THE MAIN LOOP ESSENTIALLY CLOSING THE PROGRAM.
 			break;
 		}
@@ -97,8 +81,6 @@ int main(int argc, char **argv)
     consoleExit(NULL);
 	//MARK: EXIT SPL
 	splExit();
-	//MARK: UNREGISTER ROMFS (ROM FILE SYSTEM)
-	romfsExit();
 	//MARK: RETURN AKA FINISH EXIT
     return 0;
 }
